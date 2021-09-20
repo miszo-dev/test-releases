0b5c1d5f710626c11fdfbb1897aba6bad00f98de
Miszo Radomski <miszo.radomski@gmail.com>
2021-09-20 17:54:45 +0200

    build: release configuration

--- release.config.js
+++ release.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   branches: ['main', { name: 'release-candidate', prerelease: true }],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { type: 'docs', release: false },
+          { scope: 'no-release', release: false },
+          { type: 'chore', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'test', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'build', release: 'patch' },
+          { type: 'feat', release: 'minor' },
+        ],
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     '@semantic-release/git',
