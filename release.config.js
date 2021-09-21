5dfe19446d13118eef6dd7f620b92791ee16f647
Miszo Radomski <miszo.radomski@gmail.com>
2021-09-21 15:38:31 +0200

    ci: export release rules and add more cases

--- release.config.js
+++ release.config.js
@@ -1,23 +1,27 @@
+const releaseRules = [
+  { scope: 'no-release', release: false },
+  { scope: 'release', release: false },
+  { type: 'build', release: 'patch' },
+  { type: 'ci', release: 'patch' },
+  { type: 'chore', release: 'patch' },
+  { type: 'docs', release: false },
+  { type: 'feat', release: 'minor' },
+  { type: 'fix', release: 'patch' },
+  { type: 'perf', release: 'patch' },
+  { type: 'refactor', release: 'patch' },
+  { type: 'revert', release: 'patch' },
+  { type: 'style', release: 'patch' },
+  { type: 'test', release: 'patch' },
+];
+
 module.exports = {
   branches: ['main', { name: 'release-candidate', prerelease: true }],
+  releaseRules,
   plugins: [
     [
       '@semantic-release/commit-analyzer',
       {
-        releaseRules: [
-          { scope: 'no-release', release: false },
-          { scope: 'release', release: false },
-          { type: 'build', release: 'patch' },
-          { type: 'ci', release: 'patch' },
-          { type: 'chore', release: 'patch' },
-          { type: 'docs', release: false },
-          { type: 'feat', release: 'minor' },
-          { type: 'fix', release: 'patch' },
-          { type: 'perf', release: 'patch' },
-          { type: 'refactor', release: 'patch' },
-          { type: 'style', release: 'patch' },
-          { type: 'test', release: 'patch' },
-        ],
+        releaseRules,
       },
     ],
     [
@@ -26,15 +30,17 @@ module.exports = {
         presetConfig: {
           types: [
             { type: 'build', section: 'Build System' },
-            { type: 'ci', section: 'CI' },
+            { type: 'ci', section: 'Continuous Integration' },
             { type: 'chore', section: 'Chores' },
             { type: 'docs', section: 'Documentation' },
             { type: 'feat', section: 'Features' },
             { type: 'fix', section: 'Bug Fixes' },
-            { type: 'perf', section: 'Performance' },
-            { type: 'refactor', section: 'Refactor' },
+            { type: 'perf', section: 'Performance Improvements' },
+            { type: 'refactor', section: 'Code Refactoring' },
+            { type: 'revert', section: 'Reverts' },
             { type: 'style', section: 'Styles' },
             { type: 'test', section: 'Tests' },
+            { type: 'default', section: 'Miscellaneous' },
           ],
         },
       },
